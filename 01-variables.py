464b7025bd4514d16ccf651c41ae53289a1507b5
Carlos <21000405@alumnos.utleon.edu.mx>
2025-01-13 21:37:16 -0600

    primer push

---
+++ 01-variables.py
@@ -0,0 +1,12 @@
+print('ejemplo 1')
+
+num1 = 5
+num2 = 4
+res = num1 + num2
+print(res)
+
+print('ingresa dos numeros')
+num1 = int(input('ingresa el primer numero: '))
+num2 = int(input('ingresa el primer numero: '))
+suma = num1 + num2
+print('la suma de {1} + {2} es {0}'.format(suma, num1, num2))
